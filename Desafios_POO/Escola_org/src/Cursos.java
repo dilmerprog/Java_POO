da27512ea767eb1a61085a3385f28f39deca32f8
dilmerprog <dilmernotebook9@gmail.com>
2026-05-10 19:27:59 -0300

    commit_curso.java

--- Desafios_POO/Escola_org/src/Cursos.java
+++ Desafios_POO/Escola_org/src/Cursos.java
@@ -3,13 +3,13 @@ public class Cursos {
     // Atributos
     private String nomeDoCurso;
     private int cargaHoraria;
-    private float valorMensalidade;
+    private double valorMensalidade;
 
     //Atributp de Classe
     static int totalCursosCriados = 0;
 
     //Construtor
-    public Cursos(String nomeDoCurso, int cargaHoraria, float valorMensalidade) {
+    public Cursos(String nomeDoCurso, int cargaHoraria, double valorMensalidade) {
         this.nomeDoCurso = nomeDoCurso;
         this.cargaHoraria = cargaHoraria;
         this.valorMensalidade = valorMensalidade;
@@ -22,5 +22,30 @@ public class Cursos {
         return nomeDoCurso;
     }
 
+    public void setNomeDoCurso(String nomeDoCurso) {
+        this.nomeDoCurso = nomeDoCurso;
+    }
 
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public double getValorMensalidade() {
+        return valorMensalidade;
+    }
+
+    public void setValorMensalidade(double valorMensalidade) {
+        this.valorMensalidade = valorMensalidade;
+    }
+
+    //Metodo
+    public void MostrarDadosCurso() {
+        System.out.println("Nome do curso: " + nomeDoCurso);
+        System.out.println("Carga horária: " + cargaHoraria);
+        System.out.println("Mensalidade: " + valorMensalidade);
+    }
 }
